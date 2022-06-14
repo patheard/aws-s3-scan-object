80eb8cd0b6e8635520a628fd08db8cd104cd53ca
Pat Heard <patrick.heard@cds-snc.ca>
2022-06-14 20:08:20 +0000

    feat: support SNS event source
    Update the function to support S3 and SNS event records.
    
    Also updates so that the function can process events with more
    than 1 record.

--- lambda/app.js
+++ lambda/app.js
@@ -1,9 +1,9 @@
 "use strict";
 
 /**
- * Lambda function that is invoked when an S3 object is created in a bucket.
- * This function triggers a scan of the new object for malicious content
- * and updates the object's tag with the scan status.
+ * Lambda function that can be invoked by S3 events or SNS messages.
+ * This function triggers a scan of newly created S3 objects or updates
+ * the scan status of existing S3 objects based on an SNS message payload.
  */
 
 const axios = require('axios');
@@ -12,7 +12,11 @@ const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
 
 const REGION = process.env.REGION;
 const SCAN_FILES_URL = process.env.SCAN_FILES_URL;
-const SCAN_FILES_API_KEY_PARAM_NAME = process.env.SCAN_FILES_API_KEY_PARAM_NAME;;
+const SCAN_FILES_API_KEY_PARAM_NAME = process.env.SCAN_FILES_API_KEY_PARAM_NAME;
+const SCAN_IN_PROGRESS = "IN_PROGRESS";
+const SCAN_FAILED_TO_START = "FAILED_TO_START";
+const EVENT_S3 = "aws:s3";
+const EVENT_SNS = "aws:sns";
 
 const s3Client = new S3Client({ region: REGION });
 const ssmClient = new SSMClient({ region: REGION });
@@ -23,8 +27,8 @@ const ssmClient = new SSMClient({ region: REGION });
  * every invocation.
  * @returns {Promise<{apiKey: string}>} API key to use for the scan
  */
-const initConfig = async () => {  
-  return (async () => {    
+const initConfig = async () => {
+  return (async () => {
     console.log(`Loading SSM parameter '${SCAN_FILES_API_KEY_PARAM_NAME}'`);
     const command = new GetParameterCommand({
       Name: SCAN_FILES_API_KEY_PARAM_NAME,
@@ -37,43 +41,99 @@ const initConfig = async () => {
 
 // Start config load on cold starts. This can switch to a top level `await` if we switch to ES modules:
 // https://aws.amazon.com/blogs/compute/using-node-js-es-modules-and-top-level-await-in-aws-lambda/
-const configPromise = initConfig(); 
+const configPromise = initConfig();
 
 /**
  * Lambda handler function.  This function is invoked when a new S3 object is
- * created in response to `s3:ObjectCreated:*` events.  It downloads the object
- * and triggers a scan for malicious content.
+ * created in response to `s3:ObjectCreated:*` events or when an SNS message
+ * is received with an update scan status.
  * @param {Object} event Lambda invocation event
  */
 exports.handler = async(event) => {
   const config = await configPromise;
-  const s3Object = getS3ObjectFromEvent(event);
+  let errorCount = 0;
 
-  const response = await startS3ObjectScan(`${SCAN_FILES_URL}/version`, config.apiKey, s3Object);
-  const scanStatus = response.status === 200 ? "IN_PROGRESS" : "FAILED_TO_START";
-  await tagS3Object(s3Client, s3Object, [{ Key: "scan_status", Value: scanStatus }]);
+  // Process all event records
+  for (const record of event.Records) {
+    let scanStatus = null;
+    let eventSource = getRecordEventSource(record);
+    let s3Object = getS3ObjectFromRecord(eventSource, record);
 
-  return { 
-    "status": response.status,
-    "body": `Scan status: ${scanStatus}`
+    // Start a scan of the new S3 object
+    if (eventSource === EVENT_S3) {
+      const response = await startS3ObjectScan(`${SCAN_FILES_URL}/version`, config.apiKey, s3Object);
+      scanStatus = response.status === 200 ? SCAN_IN_PROGRESS : SCAN_FAILED_TO_START;
+
+    // Get the scan status for an existing S3 object
+    } else if (eventSource === EVENT_SNS) {
+      scanStatus = record.Sns.MessageAttributes.Result.Value;
+
+    // Unknown event source
+    } else {
+      console.log(`ERROR: unsupported event source: ${JSON.stringify(record)}`);
+    }
+
+    // Tag the S3 object if we've got a scan status
+    if (scanStatus !== null) {
+      await tagS3Object(s3Client, s3Object, [{ Key: "scan_status", Value: scanStatus }]);
+    }
+
+    // Track if there were any errors with this record
+    if (scanStatus === null || scanStatus === SCAN_FAILED_TO_START) {
+      errorCount++;
+    }
+  }
+
+  return {
+    "status": errorCount > 0 ? 422 : 200,
+    "body": `"Event records processesed": ${event.Records.length}, "Errors": ${errorCount}`
   }
 }
 
 /**
+ * Determines the event record's source service.  This is either S3 or SNS.
+ * @param {Object} record Lambda invocation event record
+ * @returns {String} Event record source service, or `null` if not valid
+ */
+const getRecordEventSource = (record) => {
+  let eventSource = null;
+
+  if (record.eventSource === EVENT_S3) {
+    eventSource = EVENT_S3;
+  } else if (record.EventSource === EVENT_SNS) {
+    eventSource = EVENT_SNS;
+  }
+
+  return eventSource;
+}
+
+/**
  * Retrieves the S3 object's Bucket and key from the Lambda invocation event.
- * @param {Object} event Lambda invocation event
+ * @param {String} eventSource The source of the event record
+ * @param {Object} record Lambda invocation event record
  * @returns {{Bucket: string, Key: string}} S3 object bucket and key
  */
- const getS3ObjectFromEvent = (event) => {
-  return {
-    Bucket: event.Records[0].s3.bucket.name,
-    Key: decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' ')),
-  };
+ const getS3ObjectFromRecord = (eventSource, record) => {
+  let s3Object = null;
+
+  if (eventSource === EVENT_S3) {
+    s3Object = {
+      Bucket: record.s3.bucket.name,
+      Key: decodeURIComponent(record.s3.object.key.replace(/\+/g, ' ')),
+    };
+  } else if (eventSource === EVENT_SNS) {
+    s3Object = {
+      Bucket: record.Sns.MessageAttributes.Bucket.Value,
+      Key: record.Sns.MessageAttributes.Key.Value,
+    };
+  }
+
+  return s3Object;
 }
 
 /**
  * Starts a scan of the S3 object using the provided URL endpoint and API key.
- * @param {String} apiEndpoint API endpoint to use for the scan 
+ * @param {String} apiEndpoint API endpoint to use for the scan
  * @param {String} apiKey API authorization key to use for the scan
  * @param {{Bucket: string, Key: string}} s3Object S3 object to tag
  * @returns {Response} Axios response from the scan request
@@ -89,7 +149,7 @@ const startS3ObjectScan = async (apiEndpoint, apiKey, s3Object) => {
     });
     return response;
   } catch (error) {
-    console.log(`Failed to start scan: ${error.response}`);
+    console.log(`ERROR: failed to start scan: ${error.response}`);
     return error.response;
   }
 }
@@ -110,6 +170,6 @@ const startS3ObjectScan = async (apiEndpoint, apiKey, s3Object) => {
     const command = new PutObjectTaggingCommand({...s3Object, ...tagging});
     await s3Client.send(command);
   } catch(error) {
-    console.log(`Failed to tag S3 object: ${error}`);
+    console.log(`ERROR: failed to tag S3 object: ${error}`);
   }
 }
