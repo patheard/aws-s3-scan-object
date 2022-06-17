19e81208ab1aa6a1c66c2171ae402d97ff43327c
Pat Heard <patrick.heard@cds-snc.ca>
2022-06-17 13:28:26 +0000

    feat: only allow lambda to tag objects
    Limits object tagging in the upload bucket to the
    Lambda function only.

--- aws/s3-scan-object/s3.tf
+++ aws/s3-scan-object/s3.tf
@@ -13,6 +13,63 @@ module "upload_bucket" {
 }
 
 #
+# Bucket policy
+#
+resource "aws_s3_bucket_policy" "upload_bucket" {
+  bucket = module.upload_bucket.s3_bucket_id
+  policy = data.aws_iam_policy_document.upload_bucket.json
+}
+
+data "aws_iam_policy_document" "upload_bucket" {
+  source_policy_documents = [
+    data.aws_iam_policy_document.limit_tagging.json
+  ]
+}
+
+#
+# Only allow the lambda to add object tags
+# to upload bucket
+#
+data "aws_iam_policy_document" "limit_tagging" {
+  statement {
+    effect = "Deny"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:DeleteObjectTagging",
+      "s3:DeleteObjectVersionTagging",
+      "s3:PutObjectTagging",
+      "s3:PutObjectVersionTagging"
+    ]
+    resources = [
+      "${module.upload_bucket.s3_bucket_arn}/*"
+    ]
+    condition {
+      test     = "StringNotLike"
+      variable = "aws:PrincipalArn"
+      values   = [local.lambda_role_arn]
+    }
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [local.lambda_role_arn]
+    }
+    actions = [
+      "s3:PutObjectTagging",
+      "s3:PutObjectVersionTagging"
+    ]
+    resources = [
+      "${module.upload_bucket.s3_bucket_arn}/*"
+    ]
+  }
+}
+
+#
 # Trigger scan when file is created
 #
 resource "aws_lambda_permission" "s3_execute" {
